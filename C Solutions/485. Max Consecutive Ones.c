c71df58155068b0ebda65fd54ceb3a75fc1f6ea2
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2021-09-15 02:04:02 +0200

    Create 485. Max Consecutive Ones.c

---
+++ C Solutions/485. Max Consecutive Ones.c
@@ -0,0 +1,11 @@
+int findMaxConsecutiveOnes(int* nums, int numsSize){
+    int max = 0;
+    int curr = 0;
+    for(int i = 0; i<numsSize; i++){
+        if(nums[i] == 1) curr++;
+        else curr = 0;
+        
+        if(curr > max) max = curr;
+    }
+    return max;
+}
