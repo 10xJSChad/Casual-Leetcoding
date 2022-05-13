287fa972d1fe4821f7f1d1267ee50d2fd4fab6eb
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2022-05-13 23:54:10 +0200

    Create  Esolang Interpreters #1 - Introduction to Esolangs and My First Interpreter (MiniStringFuck).c

---
+++ CodeWars/C Solutions/Esolang Interpreters #1 - Introduction to Esolangs and My First Interpreter (MiniStringFuck).c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+char *my_first_interpreter (const char *code)
+{
+  int cell = 0;
+  int i = 0;
+  int j = 0;
+  char *output = (char *)malloc(20 * sizeof(char));;
+  
+  while(code[i]){
+    switch(code[i]){
+        case '+': cell++; break;
+        case '.': output[j] = (char)cell; j++;
+    }
+    i++;
+    output[j] = '\0';
+  }
+  return output;
+}
