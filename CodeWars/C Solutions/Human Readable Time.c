cad08e5bf74220767eefcbac49ebaec4781b8f5a
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2023-03-27 02:08:18 +0200

    Create Human Readable Time.c

---
+++ CodeWars/C Solutions/Human Readable Time.c
@@ -0,0 +1,13 @@
+char *human_readable_time (long n, char *time_string)
+{
+  int seconds = n % 60;
+  int minutes = n % 3600 / 60;
+  int hours = n / 3600;
+  
+  sprintf(time_string, "%d%d:%d%d:%d%d", 
+          hours/10, hours%10, 
+          minutes/10, minutes%10, 
+          seconds/10, seconds%10);  
+  
+	return(time_string);
+}
