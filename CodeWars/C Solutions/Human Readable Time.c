9c71ff50b395fc8b3e19adddec9630e22f2fc31e
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2023-03-27 02:08:37 +0200

    Update Human Readable Time.c

--- CodeWars/C Solutions/Human Readable Time.c
+++ CodeWars/C Solutions/Human Readable Time.c
@@ -1,5 +1,4 @@
-char *human_readable_time (long n, char *time_string)
-{
+char *human_readable_time (long n, char *time_string){
   int seconds = n % 60;
   int minutes = n % 3600 / 60;
   int hours = n / 3600;
@@ -9,5 +8,5 @@ char *human_readable_time (long n, char *time_string)
           minutes/10, minutes%10, 
           seconds/10, seconds%10);  
   
-	return(time_string);
+  return(time_string);
 }
