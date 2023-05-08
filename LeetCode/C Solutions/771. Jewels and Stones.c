32ca073772761f84fb5b64a40e393435fbdf8320
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2023-05-08 06:08:35 +0200

    Update 771. Jewels and Stones.c

--- LeetCode/C Solutions/771. Jewels and Stones.c
+++ LeetCode/C Solutions/771. Jewels and Stones.c
@@ -7,14 +7,12 @@ numJewelsInStones (
     char* jewels_lookup_array = \
         (char*) calloc(127, sizeof(char));
     
-    for (int i = 0; jewels[i] != '\0'; ++i) {
+    for (int i = 0; jewels[i] != '\0'; ++i)
         jewels_lookup_array[jewels[i]] = jewels[i];
-    }
 
-    for (int i = 0; stones[i] != '\0'; ++i) {
+    for (int i = 0; stones[i] != '\0'; ++i)
         if (jewels_lookup_array[stones[i]])
             ++ans;
-    }
 
     return ans;
 }
