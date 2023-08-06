1a13c1224ae88ecff0bdea09e2133f7c9d9a6a50
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2023-08-06 03:06:56 +0200

    Create 20. Valid Parentheses.c

---
+++ LeetCode/C Solutions/20. Valid Parentheses.c
@@ -0,0 +1,46 @@
+/*
+ * Super simple solution.
+ * You could go ahead and write one that doesn't use a stack and instead
+ * just repeatedly checks the next and previous chars, but I didn't.
+ */
+
+
+#define LPAREN   '('
+#define RPAREN   ')'
+#define LBRACKET '['
+#define RBRACKET ']'
+#define LBRACE   '{'
+#define RBRACE   '}'
+
+
+bool 
+isValid (char* s)
+{
+    int  i = 0;
+    char stack[10000] = { 0, };
+
+    do {
+        if (*s == LPAREN || *s == LBRACKET || *s == LBRACE)
+            stack[++i] = *s;
+
+        switch (*s)
+        {
+            case RPAREN:
+                if (stack[i--] != LPAREN)
+                    return false;
+                break;
+
+            case RBRACKET:
+                if (stack[i--] != LBRACKET)
+                    return false;
+                break;
+
+            case RBRACE:
+                if (stack[i--] != LBRACE)
+                    return false;
+                break;
+        }
+    } while (*s++);
+
+    return i == 0;
+}
