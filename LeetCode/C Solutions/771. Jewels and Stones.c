c91a2aca91896e93b59618a5d1be2cb46d0a0bb8
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2023-11-09 23:24:58 +0100

    Update 771. Jewels and Stones.c
    why did I use malloc for this lol

--- LeetCode/C Solutions/771. Jewels and Stones.c
+++ LeetCode/C Solutions/771. Jewels and Stones.c
@@ -1,17 +1,12 @@
-int
-numJewelsInStones(
-    char* jewels,
-    char* stones)
-{
-    int i = 0, ans = 0;
-    char* jewels_lookup_array = \
-        (char*) calloc(127, sizeof(char));
-    
-    for (int i = 0; jewels[i] != '\0'; ++i)
-        jewels_lookup_array[jewels[i]] = jewels[i];
+int numJewelsInStones(char* jewels, char* stones) {
+    char jewels_arr[123] = {0};
+    int ans = 0;
 
-    for (int i = 0; stones[i] != '\0'; ++i)
-        if (jewels_lookup_array[stones[i]])
+    while (*jewels)
+        jewels_arr[*(jewels++)] = 1;
+
+    while (*stones)
+        if (jewels_arr[*(stones++)])
             ++ans;
 
     return ans;
